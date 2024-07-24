abff484dd5c859dd7f355351d0c90fc2e36d84aa
Lucia Martinez <142288977+Lucybell652@users.noreply.github.com>
2024-07-23 23:32:53 -0600

    Update descargas.py
    Se cambió "pytube" por "pytubefix" debido a un error en las descargas

--- YTcyDLoad_v4/modulos/descargas.py
+++ YTcyDLoad_v4/modulos/descargas.py
@@ -1,22 +1,21 @@
-#Version 3
-import os
+import os 
 import time
-from pytube import YouTube, Playlist
-from moviepy.editor import *
+from pytubefix import YouTube, Playlist # Pytube para las descargas de videos...
+from moviepy.editor import * #... y MoviePy para convertir a mp3
 from modulos.sistema import (
-    ruta_fin_audio as rfa,
-    ruta_fin_video as rfv,
-    limpiar_nombre as lnom,
+    ruta_fin_audio,
+    ruta_fin_video,
+    limpiar_nombre,
     mensaje as msg,
     sep,
 )
 
 def descarga_video(link):
     yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
-    carpeta_video = rfv()
+    carpeta_video = ruta_fin_video()
     try:
-        titulo_video = lnom(yt.title) # Obtener el título...
-        autor_video = lnom(yt.author) #... y el autor del video y limpiarlos
+        titulo_video = limpiar_nombre(yt.title) # Obtener el título...
+        autor_video = limpiar_nombre(yt.author) #... y el autor del video y limpiarlos
 
         msg()
         sep()
@@ -32,10 +31,10 @@ def descarga_video(link):
 
 def descargar_video_mp3(link):
     yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
-    carpeta_audio = rfa()
+    carpeta_audio = ruta_fin_audio()
     try:
-        titulo_video = lnom(yt.title)
-        autor_video = lnom(yt.author)
+        titulo_video = limpiar_nombre(yt.title)
+        autor_video = limpiar_nombre(yt.author)
         titulo_video_limpio = titulo_video + " - " + autor_video
 
         msg()
@@ -58,52 +57,20 @@ def descargar_video_mp3(link):
 
 
 def descargar_video_playlist(link):
-    carpeta_video = rfv()
+    carpeta_video = ruta_fin_video()
     try:
         playlist = Playlist(link)
         for video_url in playlist.video_urls:
-            yt = YouTube(video_url, use_oauth=True, allow_oauth_cache=True)  # Mueve esta línea aquí
-            
-            msg()
-            sep()
-            titulo_video = lnom(yt.title)
-            autor_video = lnom(yt.author)
-            print(f"Iniciando descarga de: {titulo_video} por {autor_video}")
-            yt.streams.get_highest_resolution().download(carpeta_video)
-            print("Finalizado")
-            sep()
-            time.sleep(2)
-            msg()
+            descarga_video(video_url)  # Llama a la función descarga_video
     except Exception as e:
         print(f"La descarga falló: {str(e)}")
 
 
 def descargar_mp3_playlist(link):
-    carpeta_audio = rfa()
+    carpeta_audio = ruta_fin_audio()
     try:
         playlist = Playlist(link)
         for video_url in playlist.video_urls:
-            yt = YouTube(video_url, use_oauth=True, allow_oauth_cache=True)  # Mueve esta línea aquí
-            
-            msg()
-            sep()
-            titulo_video = lnom(yt.title)
-            autor_video = lnom(yt.author)
-            titulo_video_limpio = titulo_video + " - " + autor_video
-            print(f"Iniciando descarga de: {titulo_video} por {autor_video}")
-
-            video_stream = yt.streams.get_highest_resolution()
-            video_path = video_stream.download(carpeta_audio)
-            audio_path = os.path.join(carpeta_audio, f"{titulo_video_limpio}.mp3")
-            video_clip = VideoFileClip(video_path)
-            audio_clip = video_clip.audio
-            audio_clip.write_audiofile(audio_path)
-            video_clip.close()
-            os.remove(video_path)
-
-            print("Finalizado")
-            sep()
-            time.sleep(2)
-            msg()
+            descargar_video_mp3(video_url)
     except Exception as e:
         print(f"La descarga falló: {str(e)}")
